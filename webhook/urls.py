691d76d254c553373417c9de8a2374dfc73c49c3
nithin3119@gmail.com <nithin3119@gmail.com>
2025-03-27 23:00:33 +0530

    sheet

--- webhook/urls.py
+++ webhook/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import whatsapp_webhook, api_root
+from .views import whatsapp_webhook, api_root, google_sheets_webhook
 
 urlpatterns = [
     path("", api_root, name="api_root"),
     path("webhook/", whatsapp_webhook, name="whatsapp_webhook"),
-    
+    path("google-sheet/", google_sheets_webhook, name="google-sheet-webhook"),
 ]
